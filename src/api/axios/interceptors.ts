0fcd00369a29b7596fed23aecd5b2ce889a811fb
nathan <nhan.chu@twenty-tech.com>
2026-05-15 14:57:03 +0700

    updated axios config

--- src/api/axios/interceptors.ts
+++ src/api/axios/interceptors.ts
@@ -1,50 +1,22 @@
-import type { AxiosError, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
+import type { AxiosError, InternalAxiosRequestConfig } from 'axios';
 import ZustandPersist from 'zustand/persist';
 import { handleApiError } from './common';
 
-export interface TokenManager {
-  getToken: () => Promise<string | null>;
-  setToken: (token: string) => Promise<void>;
-  clearToken: () => Promise<void>;
-}
-
-export const setupRequestInterceptor = () => {
-  return async (config: InternalAxiosRequestConfig) => {
-    // Add auth token if available
-    const token = await ZustandPersist.getState().accessToken;
-    if (token && config.headers) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    if (config.data instanceof FormData) {
-      config.headers.delete('Content-Type');
-    }
-
-    console.info(`→ [API] ${config.method?.toUpperCase()} `, config);
-
-    return config;
-  };
+export const requestInterceptorSuccess = (config: InternalAxiosRequestConfig) => {
+  const token = ZustandPersist.getState().accessToken;
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 };
 
-export const setupResponseSuccessInterceptor = () => {
-  return (response: AxiosResponse) => {
-    console.info(`Raw response:`, response);
-    return response;
-  };
-};
+export const responseInterceptorError = (error: AxiosError) => {
+  const apiError = handleApiError(error);
+  console.info('[API Error]: ', apiError);
 
-export const setupResponseInterceptor = () => {
-  return async (error: AxiosError) => {
-    if (error) {
-      console.error(`Raw error:`, error);
-    }
+  if (apiError.statusCode === 401) {
+    ZustandPersist.getState().logout();
+  }
 
-    const apiError = handleApiError(error);
-
-    if (apiError.statusCode === 401) {
-      ZustandPersist.getState().logout();
-    }
-
-    return Promise.reject(apiError);
-  };
+  return Promise.reject(apiError);
 };
